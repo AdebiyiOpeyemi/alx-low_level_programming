93512ce8602fa2efed21aa042ec6c2b64cb0422f
AdebiyiOpeyemi <horphey4040@gmail.com>
2022-06-13 09:50:50 -0700

    initial commit of task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - update value to 98
+ * @n: pointer to set integer to
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contain task on the basic usage of pointers, arrays and strings
