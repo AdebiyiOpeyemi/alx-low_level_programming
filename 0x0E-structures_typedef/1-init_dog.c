33ebd28708b5135bbe29a54bd8f4404d23a3b65e
AdebiyiOpeyemi <horphey4040@gmail.com>
2022-07-04 14:04:03 -0700

    fixing checker flagged errors

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: variable initialize with struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d == NULL) /* check for NULL pointer */
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
 #ifndef _DOG_H
 #define _DOG_H
-
+#define NULL ((void *)0)
 /**
  * struct dog - a structure
  * @name: member
@@ -13,4 +13,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
