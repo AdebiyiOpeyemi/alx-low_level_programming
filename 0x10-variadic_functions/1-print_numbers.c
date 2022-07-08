80b712fca459be9dafc537adf7f736943a152920
AdebiyiOpeyemi <horphey4040@gmail.com>
2022-07-08 13:28:31 -0700

    fix checker flagged errors

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - print all passed numbers seaparated by separator
+ * @separator: delimiter
+ * @n: number of arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
