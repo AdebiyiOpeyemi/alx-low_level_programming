3ffa1e38c8c77bcc46ac5d1afdeb56eb6800ec5b
AdebiyiOpeyemi <horphey4040@gmail.com>
2022-07-15 13:27:06 -0700

    initial commit

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+# Singly linked list
+A single linked list is a list with a forward navigation that contains nodes, each node contains the actual data and a link or reference or pointer to the next node.
+## Files
+- 0-print_list.c - prints all elements of a linked list.
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+/**
+ * strucr list_s - singly linked list
+ * @str: malloc'ed string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linkes list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+#endif
