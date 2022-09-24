94e2ab93f5aa12199018d530e969aa13e886f6eb
AdebiyiOpeyemi <horphey4040@gmail.com>
2022-09-24 07:47:15 -0700

    Hot fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,37 +1,66 @@
 #include "hash_tables.h"
 /**
+ * add_node - adds a node to the hash table
+ * @ht: pointer to hash table
+ * @key: key
+ * @value: value
+ * @idx: index
+ * Return: 1 if it succeed or 0 otherwise
+ */
+int add_node(hash_table_t *ht, const char *key, const char *value,
+	unsigned long int idx)
+{
+	hash_node_t *node = NULL;
+	char *v;
+	char *k;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	v = strdup(value);
+	k = strdup(key);
+	if ((!v) && (!k))
+	{
+		free(node);
+		return (0);
+	}
+	node->key = k;
+	node->value = v;
+	if ((ht->array)[idx] == NULL)
+		node->next = NULL;
+	else
+		node->next = ht->array[idx];
+	ht->array[idx] = node;
+	return (1);
+}
+/**
  * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash table
  * @key: key
  * @value: value to add
  * Return: 1 if it succeeded or 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	char *k_copy;
-	char *v_copy;
+	unsigned long int idx;
 	hash_node_t *node = NULL;
-	hash_node_t *current_node = NULL;
-	unsigned long int index = 0;
+	char *v;
 
-	if (!ht)
+	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
 		return (0);
-	if (!key)
-		return (0);
-	v_copy = strdup(value);
-	k_copy = strdup(key);
-	/*Creates a key value pair node*/
-	node = malloc(sizeof(hash_node_t));
-	if (!node)
-		return (0);
-	node->key = k_copy;
-	node->value = v_copy;
-	node->next = NULL;
-	index = hash_djb2((const unsigned char *)key) % ht->size;
-	current_node = ht->array[index];
-	if (current_node == NULL)
-		/*empty cell*/
-		ht->array[index] = node;
-	else
-		ht->array[0] = node; /* added to the begining of the array*/
-	return (1);
+	idx = key_index((unsigned char *)key, ht->size);
+	node = ht->array[idx];
+	while (node && strcmp(key, node->key) != 0)
+		node = node->next;
+	if (node != NULL)
+	{
+		v = strdup(value);
+		if (!v)
+			return (0);
+		if (node->value)
+			free(node->value);
+		node->value = v;
+		return (1);
+	}
+	return (add_node(ht, key, value, idx));
 }
